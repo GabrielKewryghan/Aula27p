a32cb587618632ca9278fe23a1fa564195c34358
Gabriel Kewryghan <g.rosas@aluno.senai.br>
2024-11-28 09:36:54 -0500

    adicionando o sistema de menu e relatorios

--- atividade.py
+++ atividade.py
@@ -1,35 +1,95 @@
-Qtd_alunos = int(input("Escreva a quantidade de alunos: "))                         #quantidade de alunos
-cont = 1                                                                            #contador
+#arrays guardando as informações do usuario
+Nomes = []                                                                                  
+Notas = []                                                                                  
+Faltas = []                                                                                 
+Medias = []
+situacoes = []
 
-while cont <= Qtd_alunos:                                                           #while para cada aluno
-    print (f"\n\nDados do {cont}° aluno")                                             #identificando o aluno
+#Contadores exercendo diferentes funções
+contgeral = 0
+contAR = 0
+cont = 1   
 
-    Nome = input("\nEscreva o nome do aluno: ")                                     #pedindo o nome do aluno
-    Nt1 = float(input("\nEscreva a primeira nota do aluno: "))                      #pegando a primeira nota do aluno   
-    Nt2 = float(input("Escreva a segunda nota do aluno: "))                         #pegando a segunda nota do aluno
-    Nt3 = float(input("Escreva a terceira nota do aluno: "))                        #pegando a terceira notas do aluno
-    Nt4 = float(input("Escreva a quarta nota do aluno: "))                          #pegando a quarta notas do aluno
-    Faltas = int(input("\nEscreva a quantidade de faltas: "))                       #pedindo as faltas do aluno
+#Declarando a variavel de decisão
+Decisao = int                                                                                    
+
+#Inicio do input do usuario sobre qual decisão ele quer tomar
+print ("----------------------------------------------------------------------------------------------------------")
+while Decisao != 0:
+    print ("\nO que deseja fazer?\n\nCadastrar alunos = 1\nVer o relatorio dos alunos = 2\nSair do Menu = 0")
+    Decisao = int(input("\nEscreva o numero relacionado a ação que queria fazer(1, 2 ou 0): "))
+    print ("----------------------------------------------------------------------------------------------------------")
+
+    #Caso ele escolha a opção 1 entra na parte de cadastro de alunos
+    if Decisao == 1:
+        #Recebe quantidade de alunos, servindo como limite do while abaixo
+        Qtd_alunos = int(input("\nEscreva a quantidade de alunos que deseja cadastrar: ")) 
+        print ("----------------------------------------------------------------------------------------------------------")
+
+        #while dando inicio ao processo de cadastro de cada aluno
+        while cont <= Qtd_alunos:                                                           
+            print (f"\n\nDados do {cont}° aluno")                                          
+
+            #guardando os valores fornecidos pelo usuario
+            Nome = input("\nEscreva o nome do aluno: ")                                     
+            Nt1 = float(input("\nEscreva a primeira nota do aluno: "))                      
+            Nt2 = float(input("Escreva a segunda nota do aluno: "))                         
+            Nt3 = float(input("Escreva a terceira nota do aluno: "))                        
+            Nt4 = float(input("Escreva a quarta nota do aluno: "))                          
+            QtdFaltas = int(input("\nEscreva a quantidade de faltas: "))                    
+            
+            #calculo da media
+            Media = (Nt1+Nt2+Nt3+Nt4)/4                                                     
+
+            #conjunto de condições para cada caso de aprovação, reprovação e caso ocorro um erro nos valores
+            if Media >= 8:                                                                  
+                situação = "aprovado"                                                       
+            elif Media >= 5:                                                               
+                ntRec = float(input("\nEscreva a nota de recuperação do aluno: "))          
+                if ntRec < (10 - Media):                                                    
+                    situação = "Reprovado"                                                  
+                else:                                                                       
+                    situação = "aprovado"                                                   
+            else:                                                                           
+                situação = "Reprovado"      
+
+            #Caso as faltas sejam maior que 30 o aluno é reprovado imediatamente
+            if QtdFaltas > 30:                                                              
+                situação = "Reprovado" 
+
+            #Caso os dados não batem, a situação é definido como alterada
+            if Media > 10 or Media < 0:                                                     
+                situação = "Dados invalidos"                                                
+            
+            #arredondando a media
+            Media=round(Media,2)                                                            
+
+            #Cada array recebe um valor, facilitando o guardar de dados e principalmente a impressão na tela
+            Nomes.append (f"Nome: {Nome}")
+            Notas.append (f"Notas: {Nt1}, {Nt2}, {Nt3}, {Nt4}")
+            Faltas.append (f"faltas: {QtdFaltas}")
+            Medias.append (f"Media: {Media}")
+            situacoes.append (f"situação: {situação}")
+
+            cont += 1                                               #incremento do array do cadastro
+            contgeral +=1                                           #incremento do contador dos arrays
+            print ("--------------------------------------------------------------------------------------------------")
+
+    #Caso o usuario escolha a segunda opção, é mostrado o relatorio de todos os alunos cadastrados
+    elif Decisao == 2:
+        #While utiliza um contando da operação e compara com o limite de contador geral de todos os alunos cadastrados
+        while contAR < contgeral:
+            print (Nomes[contAR],"\n",Notas[contAR],"\n",Faltas[contAR],"\n",Medias[contAR],"\n",situacoes[contAR])
+            print ("-----------------------------------------------")
+            contAR += 1
     
-    Media = (Nt1+Nt2+Nt3+Nt4)/4                                                     #calculo da media
-
-    if Media >= 8:                                                                  #caso media alcance a nota necessario para ser aprovado
-        situação = "aprovado"                                                       #caso o aluno passe, situação recebe true
-    elif Media >= 5:                                                                #caso a media alcance a nota de recuperação
-        ntRec = float(input("\nEscreva a nota de recuperação do aluno: "))          #variavel com a nota de recuperação
-        if ntRec < (10 - Media):                                                    #caso o aluno não alcance a nota de recuperação necessaria
-            situação = "Reprovado"                                                  #nesse caso, situação recebe False
-        else:                                                                       #em qualquer outro caso o aluno é aprovado
-            situação = "aprovado"                                                   #situação recebe True
-    else:                                                                           #em qualquer outro caso o aluno é reprovado
-        situação = "Reprovado"                                                      #situação recebe False
+    #Caso o usario escolha 0 a operação é encerrada
+    elif Decisao == 0:
+        print ("tudo bem, o programa vai ser encerrado agora.")
     
-    if Faltas > 30:                                                                 #Caso as faltas sejam maior que 30 o aluno é reprovado imediatamente
-        situação = "Reprovado"                                                      #situação recebe False
+    #Caso o usuario escreva algum outro valor, a mensagem de erro aparece
+    else:
+        print ("numero invalido, os numeros de operações disponiveis são: 1,2 ou 0")   
     
-    Media=round(Media,2)                                                            #arredondando a media
-
-    #Relatorio do aluno
-    print (f"\nNome: {Nome}\n\nNotas: {Nt1}, {Nt2}, {Nt3}, {Nt4}\nfaltas: {Faltas}\nMedia: {Media}\nSituação: {situação}")
-
-    cont += 1                                                                       #incremeto do contador
+    #O contador do cadastro de usuario é resetado
+    cont = 0
